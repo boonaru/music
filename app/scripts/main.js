21695d643e11aab3bdf0f654d99fb3d5a2288520
Robert <robert.romore@meredith.com>
2013-10-30 14:16:04 -0500

    Comment out the loader settings for now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -41,30 +41,31 @@ window.music = {
               songsview.gotoPage(n);
             },
             'play/*url': function(url) {
-              console.log(url);
+              //console.log(url);
+              songsview.player.play(url);
             }
           }
         });
 
-        var opts = {
-          lines: 9, // The number of lines to draw
-          length: 2, // The length of each line
-          width: 2, // The line thickness
-          radius: 7, // The radius of the inner circle
-          corners: 1, // Corner roundness (0..1)
-          rotate: 0, // The rotation offset
-          direction: 1, // 1: clockwise, -1: counterclockwise
-          color: '#2a6496', // #rgb or #rrggbb or array of colors
-          speed: 1, // Rounds per second
-          trail: 60, // Afterglow percentage
-          shadow: false, // Whether to render a shadow
-          hwaccel: true, // Whether to use hardware acceleration
-          className: 'spinner', // The CSS class to assign to the spinner
-          zIndex: 2e9, // The z-index (defaults to 2000000000)
-          top: 'auto', // Top position relative to parent in px
-          left: 'auto' // Left position relative to parent in px
-        };
-        var target = document.getElementById('loader');
+        // var opts = {
+        //   lines: 9, // The number of lines to draw
+        //   length: 2, // The length of each line
+        //   width: 2, // The line thickness
+        //   radius: 7, // The radius of the inner circle
+        //   corners: 1, // Corner roundness (0..1)
+        //   rotate: 0, // The rotation offset
+        //   direction: 1, // 1: clockwise, -1: counterclockwise
+        //   color: '#2a6496', // #rgb or #rrggbb or array of colors
+        //   speed: 1, // Rounds per second
+        //   trail: 60, // Afterglow percentage
+        //   shadow: false, // Whether to render a shadow
+        //   hwaccel: true, // Whether to use hardware acceleration
+        //   className: 'spinner', // The CSS class to assign to the spinner
+        //   zIndex: 2e9, // The z-index (defaults to 2000000000)
+        //   top: 'auto', // Top position relative to parent in px
+        //   left: 'auto' // Left position relative to parent in px
+        // };
+        // var target = document.getElementById('loader');
         // var spinner = new Spinner(opts);
 
         // $(document).ajaxStart(function() {
